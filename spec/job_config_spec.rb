a1246ae35b00c23b29d0a5a3ec457cf8f70d1eae
Eric Steen <ericsteen1@gmail.com>
2011-05-12 02:36:51 -0700

    test for config path

--- spec/job_config_spec.rb
+++ spec/job_config_spec.rb
@@ -3,13 +3,13 @@ require 'job_config'
 
 describe JobConfig do
 
+  it "should return the right path" do
+    JobConfig.config_path.should == "config/external_resources"
+  end
+
   it "loads the yaml file" do
     config = JobConfig.new(:filename => 'patent.yml', :env => "test")
     config.data.should include "test"=>{"foo"=>"bar"}
   end
-
-#  it "should return the right path" do
-#    JobConfig.config_path.should == "../config/external_resources"
-#  end
 
 end
